fd36d3c46fd7645275633e32a575d60e7ae36f23
kevinnn2 <73437183+kevinnn2@users.noreply.github.com>
2020-12-01 17:10:03 +0700

    Add files via upload

---
+++ remote/APIutil.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.remote;
+
+public class APIutil {
+
+    private APIutil() {}
+
+    public static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+
+    public static APIService getAPIService() {
+
+        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
+    }
+
+}
